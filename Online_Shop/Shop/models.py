a13abd7d44c8e50ad08c463f278797cd280c4611
NOPe <arvipad002@gmail.com>
2021-08-03 22:43:21 +0530

    Improved home page
    Worked a bit on the home page but a lot of things didnt work out so scrapped them and commiting only things that work , moving on for now.

--- Online_Shop/Shop/models.py
+++ Online_Shop/Shop/models.py
@@ -4,4 +4,5 @@ class Product(models.Model):
     productname = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.CharField(max_length=5000, null=True, blank=True)
+    image = models.CharField(max_length=50000)
+    
