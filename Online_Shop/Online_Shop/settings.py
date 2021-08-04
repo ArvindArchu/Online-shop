4fb94421353c0e5ccdcb226788b695f426243d1a
NOPe <arvipad002@gmail.com>
2021-08-04 22:36:11 +0530

    (imp)Added user registration and login app
    Huge commit so make sure to pull and merge

--- Online_Shop/Online_Shop/settings.py
+++ Online_Shop/Online_Shop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'Shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
